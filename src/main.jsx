0346b70a25595564978971bc33101a9dd4965418
Sarah <sarah001160@gmail.com>
2025-04-30 00:03:02 +0800

    test 設置

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 // 入口點
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom"; // 載入 BrowserRouter
 //import './index.css'
 import "./main.sass";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>
 );
